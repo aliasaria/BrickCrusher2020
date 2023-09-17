0a471ef48de2d0dc60ce3e6329d6da6d8e5cf2eb
ali asaria <aliasaria@users.noreply.github.com>
2023-09-16 21:25:19 -0400

    improved cutscene text

--- Source/cutscene_text.lua
+++ Source/cutscene_text.lua
@@ -1,15 +1,16 @@
 CUTSCENE_TEXT = {
-    "Welcome to Planet Brika, a peaceful planet of brick people.\fWe appreciate your help in defending our planet.\fLet's test out your systems on the following bricks.\fRemember, press Â to release the ball.\fPress ß to shoot bullets if you have the gun.\fYou can also use the ┬ and ┴ buttons on your D-pad to do the same actions.",
-    "Looking good! We need your help to clear rubble.\fWe have a big mess from the last Brikalon attack\f",
-    "Great job, BrickCrusher2020! You're a pro. Now we just ...\fhuh? ...\fOH NO!!! ...\fYou came just in time!!! ...\fThe evil BRICKALONS must have detected your presense.\fPlease help defend our planet from their merciless attacks.\fBut watch out. Their ship is protected by impenetrable metal bricks.",
+    "Welcome back to Softland, BrickCrusher,\fwe appreciate your return from retirement.\fWe need your help to defend the city from the Brickalons\fLet's start by testing your systems on the following bricks.\fRemember, press Â to release the breaker ball.\fPress ß to shoot bullets if you have the gun.\fYou can also use the ┬ and ┴ buttons on your D-pad to do the same actions.",
+    "All systems looking good, pilot! We need your help to clear rubble.\fWe have a big mess from the last Brikalon attack\f",
+    "Great job, BrickCrusher! You're a pro. Now we just ...\fhuh? ...\fOH NO!!! ...\fYou came just in time!!! ...\fThe evil BRICKALONS must have detected your presense.\fPlease help defend the city from their merciless attacks.\fBut watch out. Their ship is protected by impenetrable metal bricks.",
     "You destroyed their ship!\fHowever, their leader, BRICKMALEON escaped! He plans to crush our city.\fHelp us!!!!!",
     "Brickmaleon is defeated. Nice! Now let's rebuild our city.",
-    "A faction of Brikalons have captured our mayor and trapped her in a cage of metal.\fPlease help us free her.",
-    "This is the last level. If we clear these bricks, we can rebuild our city.\fThank you for your help, BrickCrusher2020!",
+    "A faction of Brikalons have captured our mayor and trapped her.\fPlease help us free Mayor Marshmallow and save the city.",
+    "We're almost done, pilot. If we clear these bricks, we can finally rebuild.\fThank you for your help!",
 }
 
 GAME_OVER_TEXT = "You tried. But you failed.\fThe dream is over.\fPress A to restart."
 
 OUCH_TEXT = "Ouch.\nDon't give up yet."
 
-YOU_WIN_TEXT = "You defeated the Brikalons and saved Brika!\fThank you.\fPress A to restart."
+YOU_WIN_TEXT =
+"YOU WIN! You defeated the Brikalons and saved Softland!\fYou will be remembed as a hero for all time.\fPress A to play again."
